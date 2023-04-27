8e252531fcd7da1328413d56b4d50d50b6a01b57
JoseParra28 <iliftandcook@gmail.com>
2023-04-27 20:03:35 +0000

    start mobile rsponsiveness

--- assets/script.js
+++ assets/script.js
@@ -24,33 +24,67 @@ ScrollReveal({
 //  * typing text *//
 
  const body = document.querySelector('body');
- const textTransform = document.getElementById('text-2');
+//  const textTransform = document.getElementById('text-2');
 
  const toggle = document.getElementById('toggle');
  toggle.onclick = function(){
     body.classList.toggle('active');
     toggle.classList.toggle('active'); 
     text.classList.toggle('active'); 
+    textT.classList.toggle('active'); 
     parra.classList.toggle('active');
     parraA.classList.toggle('active');
     pArra.classList.toggle('active');
     paRra.classList.toggle('active');
  }
 
-const text = document.querySelector(".text-2");
+// const text = document.querySelector(".text-2");
 
-const textLoad = () => {
-    setTimeout(() => {
-        text.textContent = "Developer";
-    }, 0)
-    setTimeout(() => {
-        text.textContent = "Designer";
-    }, 4000)
-    
+// textLoad();
+// setInterval(textLoad, 12000);
+// ---------------------------------------------------typing text
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Developer", "Designer", "Addict :/"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
 }
 
-textLoad();
-setInterval(textLoad, 12000);
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
 
 
 const container = document.querySelector(".container");
